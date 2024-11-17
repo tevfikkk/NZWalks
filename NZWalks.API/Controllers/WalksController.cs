a2d7abec05616a5dab3dfc8d750f6867616593db
Tevfik <79837579+tevfikkk@users.noreply.github.com>
2024-11-17 19:54:58 +0300

    filter, sort, pagination

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -31,12 +31,17 @@ namespace NZWalks.API.Controllers
         }
 
         // GET Walks
-        // GET: https://localhost:portnumber/api/walks
+        // GET: /api/walks?filterOn=Name&filterQuery=Track&sortBy=Name&isAscending=true&pageNumber=1&pageSize=10
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn,
+                                                [FromQuery] string? filterQuery,
+                                                [FromQuery] string? sortBy,
+                                                [FromQuery] bool? isAscending,
+                                                [FromQuery] int pageNumber = 1,
+                                                [FromQuery] int pageSize = 100)
         {
             // Fetch walks
-            var walksDomainModel = await _walkRepository.GetAllAsync();
+            var walksDomainModel = await _walkRepository.GetAllAsync(filterOn, filterQuery, sortBy, isAscending ?? true, pageNumber, pageSize);
 
             // Map Domain Model to DTO and then return it as a response
             return Ok(_mapper.Map<List<WalkDto>>(walksDomainModel));
